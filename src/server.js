00161f19b4a7a041d8e8f46de515cc028e902d77
Gustavo Pereira de Souza <gustavopereira@sistemafieto.com.br>
2019-11-11 16:34:45 -0300

    Backend SESI Eventos

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://sesieventos:sesiti123@sesieventos-ggddh.mongodb.
 });
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(3333);
